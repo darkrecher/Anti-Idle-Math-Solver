9d8ddf5e5802fbc8521f1adc8e526ed6e731ab1f
darkrecher <recher_burp@yahoo.fr>
2013-03-21 22:34:22 +0100

    docstring enisolvr fini

--- enisolvr/enisolvr.py
+++ enisolvr/enisolvr.py
@@ -1,11 +1,81 @@
 ﻿# -*- coding: utf-8 -*-
 
+""" 
+créé par Réchèr. Licence CC-BY ou Art Libre.
+https://github.com/darkrecher/Anti-Idle-Math-Solver
+je prends les bitcoins : 12wF4PWLeVAoaU1ozD1cnQprSiKr6dYW1G
+
+module contenant la classe EnigmaSolver, qui résout les énigmes et renvoie la
+bonne réponse.
+
+Mode d'emploi
+=============
+
+Rien de bien compliqué. Faut instancier un EnigmaSolver, puis exécuter la
+fonction solve, en passant en paramètre le texte d'une énigme.
+
+Si l'énigme est solvable, la fonction renvoie une chaîne de caractères,
+correspondant à la bonne réponse. Sinon, la fonction renvoie None. C'est tout.
+
+Fonctionnement interne
+======================
+
+On commence par virer toutes les virgules du texte d'énigme. À cause de ces
+cons d'américains qui écrivent les grands nombres sous la forme 1,234,567.
+
+Il y a 3 sortes d'énigmes :
+
+1) 
+Si ça finit par les caractères "égal", puis "point d'interrogation", c'est une
+énigme de calcul. Par exemple : "2+2=?". Pour résoudre cette énigme, on 
+évalue le texte qui se trouve avant le signe égal.
+
+2)
+Si l'énigme ne comporte pas de signe "égal", c'est une énigme de comparaison.
+Par exemple : "3*5?17". Pour résoudre cette énigme, on sépare le texte en 2 :
+la partie avant le point d'interrogation, et celle après. (Si le texte ne 
+comporte pas de point d'interrogation, l'énigme n'est pas solvable). On évalue
+les deux parties de texte, et on les compare. La comparaison (plus petit, 
+égal ou plus grand) est la réponse à l'énigme.
+
+3)
+Si l'énigme comporte un point d'interrogation, puis un signe "égal", avec du
+texte entre chacun de ces caractères, c'est une énigme de recherche 
+d'opérateur. Par exemple : "5?7=35". On évalue le premier opérande (la partie
+de texte situé avant le point d'interrogation), le deuxième (texte situé 
+entre le point d'interrogaton et le "égal") et le résultat (texte situé après
+le "égal").
+Ensuite on essaie toutes les possibilités d'opérateur. On calcule 
+opérande_1 + opérande_2. Si c'est égal au résultat, la réponse à l'énigme est
+"+". On fait pareil avec les opérateurs de soustraction, multiplication et
+division. Si aucun de ces calculs n'est égal au résultat, l'énigme n'est pas
+solvable.
+
+4)
+Si l'énigme ne rentre dans aucun des 3 cas précédemment cités, elle n'est pas
+solvable.
+
+Pour les 3 sortes d'énigme, on doit évaluer des parties de texte. Cette action
+est effectuée par la foncton _interpret_enigma_part. On doit calculer la 
+valeur numérique représenté par l'expression de la partie de texte en 
+question. Ça peut être quelque chose de très simple, comme "42", ou un peu 
+plus compliqué, comme "(123-99)*74". Ces parties de texte à évaluer ne sont 
+censées contenir que des chiffres, les 4 opérateurs, et des parenthèses.
+Il n'est pas censé y avoir de point d'interrogation ou de signe "égal".
+
+Pour évaluer ces parties de texte, on le fait à la gros bourrin : la fonction
+python "eval", et vlan dans la gueule. Si ça pète en cours de route, on
+considère que la partie de texte ne peut pas être évaluée, et que donc 
+l'énigme n'est pas solvable. Sinon, c'est cool.
+Avant d'exécuter la fonction "eval", on ne vérifie même pas si la partie de
+texte ne contient que les caractères autorisés. On essaie direct, donc c'est
+vraiment du bon gros bourrin. Mais j'aime ça.
+"""
+
 from log import log, msg
 
 
 class EnigmaSolver():
-    """ 
-    """
     
     DICT_ANSWER_FROM_COMPARRISON = {
         -1 : "<",
