4651be76964cef1d8815864693b66a34e3e2c5cb
darkrecher <recher_burp@yahoo.fr>
2013-04-05 19:29:02 +0200

    Voila, ça marche. Même pas besoin de changer la config.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ https://github.com/darkrecher/Anti-Idle-Math-Solver
 je prends les bitcoins : 12wF4PWLeVAoaU1ozD1cnQprSiKr6dYW1G
 
 Fichier de code principal de Anti-Idle Math Solver.
-Pour une description détaillée de ce que fait ce script, voir fichier
+Pour une description détaillée de ce que fait ce script, voir le fichier
 doc_conception.md dans le repository sus-cité.
 """
 
